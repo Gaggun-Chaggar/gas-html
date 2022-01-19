9cfbdeacebd4ce594f50ea868756ca8fdc0df422
Gaggun Chaggar <gaggunchaggar@gmail.com>
2022-01-19 23:28:56 +0000

    Updated es5 build with shebang

--- scripts/build.es5.sh
+++ scripts/build.es5.sh
@@ -1,10 +1,8 @@
+#!/bin/sh
+
 mkdir ./es5
 touch ./es5/index.ts
 cp ./src/index.ts ./es5/index.ts
-if [[ "$OSTYPE" == "darwin"* ]]; then
-  sed -i '' 's/export //g' ./es5/index.ts
-else
-  sed -i 's/export //g' ./es5/index.ts
-fi
+sed -i -e 's/export //g' ./es5/index.ts
 tsc ./es5/*.ts --outDir ./dist/es5
 rm -r ./es5
